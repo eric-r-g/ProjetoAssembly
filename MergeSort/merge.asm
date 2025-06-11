ad68e60792bf8041d0de1667d0a0e52addb610fd
Bibicm2006 <99053329+Bibicm2006@users.noreply.github.com>
2025-06-11 06:03:27 -0300

    Update merge.asm

--- MergeSort/merge.asm
+++ MergeSort/merge.asm
@@ -3,10 +3,34 @@
 	array: .space 400
 	l_array: .space 200
 	r_array: .space 200
-	
+	msg1: .asciiz "Quantos números deseja ordenar?\n"
+	msg2: .asciiz "Digite um número: "
+	msg3: .asciiz "Array ordenado:\n"
+	.align 2
+	array: .space 400
+	space: .asciiz " "
 .text
 .globl main
-
+scan_array:
+	beq $t1, $s1, merge     # se índice == tamanho, encerra loop
+	
+	# Mostrar mensagem
+	la $a0,msg2
+	li $v0, 4
+	syscall
+	
+	# Ler número do usuário
+	li $v0, 5
+	syscall
+	move $t3, $v0         # salva número em $t3
+	
+	# Calcular endereço array[indice]
+	mul $t4, $t1, 4   # distancia percorrida = índice * 4 (com alinhamento garantido)
+	add $t5, $s0, $t4     # endereço = base + distancia percorrida
+	
+	sw $t3, 0($t5)    # Armazenar número no endereço
+	addi $t1, $t1, 1   # Incrementar índice
+	j scan_array   #jump para o inicio de scan_array
 merge:
 	li $t0 0    # indice do array
 	li $t1 0    # indice do l_array
@@ -39,6 +63,47 @@ LOOP_PRINCIPAL:
 	
 EXIT_LP:
 
-main:
+imprimir:
+	li $t1,0
+	la $a0,msg3
+	li $v0,4
+	syscall
+print_array:
+	beq $t1, $s1, end     # se índice == tamanho, encerra loop
+	    
+	# Carregar o número do array
+	mul $t4, $t1, 4
+	add $t5, $s0, $t4
+	lw $a0, 0($t5)
 	
+	# Imprimir o número
+	li $v0, 1
+	syscall
+	
+	# Imprimir um espaço
+	li $v0, 4
+	la $a0, space
+	syscall
+	
+	# Próximo índice
+	addi $t1, $t1, 1
+	j print_array
+	
+end:
+	li $v0, 10
+	syscall
+	
+main:
+	la $s0,array    # $s0 = endereço inicial do array
+	li $t1,0	    # indice = 0
+    	
+   	# Mostrar mensagem para pedir tamanho do array
+   	li $v0, 4
+    	la $a0, msg1
+    	syscall
+	
+   	# Ler tamanho do array
+    	li $v0, 5
+    	syscall
+   	move $s1, $v0       # $s1 = tamanho do array (n)
 	
